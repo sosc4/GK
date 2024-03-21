b16ba9b3cf0124d35773d49bedd2aaefcba5e551
Oskar Stasiak <oski90091@gmail.com>
2024-03-21 17:52:05 +0100

    ✨ Implemented 9 button action

--- Lab3/main.py
+++ Lab3/main.py
@@ -47,7 +47,7 @@ def main():
             septagon = utils.create_septagon(septagon_center, config.SEPTAGON_SIZE)
 
         if pressed[pygame.K_2]['pressed']:
-            septagon = utils.create_septagon(septagon_center, 2 * config.SEPTAGON_SIZE,
+            septagon = utils.create_septagon(septagon_center, 1.75 * config.SEPTAGON_SIZE,
                                              angle=-90)
 
         if pressed[pygame.K_3]['pressed']:
@@ -55,7 +55,7 @@ def main():
                                              angle=180, vertical_stretch=2)
 
         if pressed[pygame.K_4]['pressed']:
-            septagon = utils.create_septagon(septagon_center, 2 * config.SEPTAGON_SIZE,
+            septagon = utils.create_septagon(septagon_center, 1.75 * config.SEPTAGON_SIZE,
                                              x_skew=0.5)
 
         if pressed[pygame.K_5]['pressed']:
@@ -67,7 +67,7 @@ def main():
             septagon = [(x, y + top_difference) for x, y in septagon]
 
         if pressed[pygame.K_6]['pressed']:
-            septagon = utils.create_septagon(septagon_center, 2 * config.SEPTAGON_SIZE,
+            septagon = utils.create_septagon(septagon_center, 1.75 * config.SEPTAGON_SIZE,
                                              x_skew=0.5, angle=90)
 
         if pressed[pygame.K_7]['pressed']:
@@ -78,6 +78,15 @@ def main():
             septagon_center = (config.WINDOW_WIDTH // 2, (config.WINDOW_HEIGHT // 2) + 100)
             septagon = utils.create_septagon(septagon_center, config.SEPTAGON_SIZE,
                                              angle=45, horizontal_stretch=2.0)
+
+        if pressed[pygame.K_9]['pressed']:
+            septagon = utils.create_septagon(septagon_center, 1.75 * config.SEPTAGON_SIZE,
+                                             angle=180, x_skew=0.5)
+
+            top_x = max(septagon, key=lambda point: point[0])[0]
+
+            top_difference = 590 - top_x  # TODO: Calculate this properly
+            septagon = [(x + top_difference, y) for x, y in septagon]
 
         if septagon:
             pygame.draw.polygon(win, config.COLOR_OF_USE, septagon, 2)
