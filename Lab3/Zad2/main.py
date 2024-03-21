86f3d65810ef0de6343e52ba790f842d544850d5
Oskar Stasiak <oski90091@gmail.com>
2024-03-21 19:09:19 +0100

    ✨ Pygame now draws rectangle and two rectangles according to task assigned.

--- Lab3/Zad2/main.py
+++ Lab3/Zad2/main.py
@@ -16,3 +16,12 @@ while run:
             run = False
 
     screen.fill((255, 255, 255))
+
+    # rectangle
+    pygame.draw.rect(screen, BLUE, [100, 150, 200, 100], 0)
+
+    # triangles
+    pygame.draw.polygon(screen, BLUE, [[200, 150], [150, 50], [250, 50]], 0)
+    pygame.draw.polygon(screen, BLUE, [[200, 250], [150, 350], [250, 350]], 0)
+
+    pygame.display.flip()
