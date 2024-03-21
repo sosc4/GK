8c86475b0c379c3dc31b6fe0ad1b052c965f161d
Oskar Stasiak <oski90091@gmail.com>
2024-03-21 15:13:48 +0100

    ✨ Implemented skew method

--- Lab3/main.py
+++ Lab3/main.py
@@ -2,6 +2,7 @@ import math
 import time
 from typing import Optional
 
+import numpy as np
 import pygame
 
 pygame.init()
@@ -36,16 +37,32 @@ def rotate_point(center: tuple[float | int, float | int],
     return qx, qy
 
 
+def skew_matrix(x_skew: float, y_skew: float) -> np.ndarray:
+    return np.array([
+        [1, x_skew, 0],
+        [y_skew, 1, 0],
+        [0, 0, 1]
+    ])
+
+
+def affine_transform(point: tuple[float, float], matrix: np.ndarray) -> tuple[float, float]:
+    point = np.array([*point, 1.0])
+    transformed_point = matrix @ point
+
+    return transformed_point[0], transformed_point[1]
+
+
 def draw_septagon(surface: pygame.Surface,
                   color: tuple[int, int, int],
                   center: tuple[float | int, float | int],
                   size: int | float,
                   *,
                   angle: Optional[int | float] = 0,
-                  vertical_stretch: Optional[float] = 1.0):
-    """
-    Draw a septagon on a given surface, with an option to rotate and vertically stretch.
-    """
+                  vertical_stretch: Optional[float] = 1.0,
+                  x_skew: Optional[float] = 0.0,
+                  y_skew: Optional[float] = 0.0):
+    skew_mat = skew_matrix(x_skew, y_skew)  # Create skew matrix
+
     coordinates = []
     for i in range(7):
         original_x = center[0] + size * math.cos(math.radians(i * (360 / 7) - 90))
@@ -53,7 +70,9 @@ def draw_septagon(surface: pygame.Surface,
 
         original_y = center[1] + (original_y - center[1]) * vertical_stretch
 
-        rotated_point = rotate_point(center, (original_x, original_y), math.radians(angle))
+        skewed_x, skewed_y = affine_transform((original_x, original_y), skew_mat)
+
+        rotated_point = rotate_point(center, (skewed_x, skewed_y), math.radians(angle))
         coordinates.append(rotated_point)
 
     pygame.draw.polygon(surface, color, coordinates, 2)
@@ -100,5 +119,9 @@ while run:
     if pressed[pygame.K_3]['pressed']:
         draw_septagon(win, COLOR_OF_USE, septagon_center, septagon_size,
                       angle=180, vertical_stretch=1.5)
+
+    if pressed[pygame.K_4]['pressed']:
+        draw_septagon(win, COLOR_OF_USE, septagon_center, septagon_size,
+                      x_skew=0.3)
 
     pygame.display.update()
