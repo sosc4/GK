503bd8fecbb7776e94f0629dcdf72da800683d54
Oskar Stasiak <oski90091@gmail.com>
2024-03-21 16:57:20 +0100

    🔨 Refactored septagon drawing and added main() function

--- Lab3/main.py
+++ Lab3/main.py
@@ -4,59 +4,65 @@ import pygame
 
 from source import config, utils
 
-pygame.init()
-win = pygame.display.set_mode((config.WINDOW_WIDTH, config.WINDOW_HEIGHT))
-pygame.display.set_caption("Lab3 - Oskar Stasiak")
 
-buttons = [pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, pygame.K_5, pygame.K_6, pygame.K_7, pygame.K_8, pygame.K_9]
-pressed = {button: {'pressed': False, 'at': None} for button in buttons}
+def main():
+    pygame.init()
+    win = pygame.display.set_mode((config.WINDOW_WIDTH, config.WINDOW_HEIGHT))
+    pygame.display.set_caption("Lab3 - Oskar Stasiak")
 
-septagon_surface = pygame.Surface((400, 400), pygame.SRCALPHA)
+    buttons = [pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, pygame.K_5, pygame.K_6, pygame.K_7, pygame.K_8,
+               pygame.K_9]
+    pressed = {button: {'pressed': False, 'at': None} for button in buttons}
 
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+    septagon_surface = pygame.Surface((config.SURFACE_WIDTH, config.SURFACE_HEIGHT),
+                                      pygame.SRCALPHA)
+    septagon_center = (config.WINDOW_WIDTH // 2,
+                       config.WINDOW_HEIGHT // 2)
 
-    win.fill("white")
-    pygame.draw.rect(win, config.YELLOW, (10, 45, 580, 545))
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-    keys = pygame.key.get_pressed()
-    for button in buttons:
-        if not keys[button]:
-            continue
+        win.fill("white")
+        pygame.draw.rect(win, config.YELLOW, (10, 45, 580, 545))
 
-        at = pressed[button]['at']
-        if at is None or time.time() - at > 0.5:
-            pressed[button]['at'] = time.time()
-            pressed[button]['pressed'] = not pressed[button]['pressed']
+        keys = pygame.key.get_pressed()
+        for button in buttons:
+            if not keys[button]:
+                continue
 
-            for other_button in buttons:
-                if other_button != button:
-                    pressed[other_button]['pressed'] = False
+            at = pressed[button]['at']
+            if at is None or time.time() - at > 0.5:
+                pressed[button]['at'] = time.time()
+                pressed[button]['pressed'] = not pressed[button]['pressed']
 
-    septagon_center = (
-        config.WINDOW_WIDTH // 2,
-        config.WINDOW_HEIGHT // 2
-    )
-    if pressed[pygame.K_1]['pressed']:
-        utils.draw_septagon(win, config.COLOR_OF_USE, septagon_center, config.SEPTAGON_SIZE)
+                for other_button in buttons:
+                    if other_button != button:
+                        pressed[other_button]['pressed'] = False
 
-    if pressed[pygame.K_2]['pressed']:
-        utils.draw_septagon(win, config.COLOR_OF_USE, septagon_center, 2 * config.SEPTAGON_SIZE,
-                            angle=-90)
+        septagon = None
+        if pressed[pygame.K_1]['pressed']:
+            septagon = utils.create_septagon(septagon_center, config.SEPTAGON_SIZE)
 
-    if pressed[pygame.K_3]['pressed']:
-        utils.draw_septagon(win, config.COLOR_OF_USE, septagon_center, config.SEPTAGON_SIZE,
-                            angle=180, vertical_stretch=2)
+        if pressed[pygame.K_2]['pressed']:
+            septagon = utils.create_septagon(septagon_center, 2 * config.SEPTAGON_SIZE,
+                                             angle=-90)
 
-    if pressed[pygame.K_4]['pressed']:
-        utils.draw_septagon(win, config.COLOR_OF_USE, septagon_center, 2 * config.SEPTAGON_SIZE,
-                            x_skew=0.5)
+        if pressed[pygame.K_3]['pressed']:
+            septagon = utils.create_septagon(septagon_center, config.SEPTAGON_SIZE,
+                                             angle=180, vertical_stretch=2)
 
-    if pressed[pygame.K_5]['pressed']:
-        utils.draw_septagon(win, config.COLOR_OF_USE, septagon_center, config.SEPTAGON_SIZE,
-                            horizontal_stretch=2)
+        if pressed[pygame.K_4]['pressed']:
+            septagon = utils.create_septagon(septagon_center, 2 * config.SEPTAGON_SIZE,
+                                             x_skew=0.5)
 
-    pygame.display.update()
+        if septagon:
+            pygame.draw.polygon(win, config.COLOR_OF_USE, septagon, 2)
+
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
